84ae934d4c0d58a2d6ddb3b777e5b7b5278013b8
c4todev <c4toedm@hotmail.com>
2024-02-08 18:43:35 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/airbnb-landing-clone/",
   plugins: [react()],
-  base: "/airbnb-clone-frontend/",
 });
